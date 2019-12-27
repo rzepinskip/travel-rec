23c6b1c7994cf8b640b46ae2d73303af5f868191
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2019-12-27 12:35:34 +0100

    Add activity snippet

--- travelrec/similarity.py
+++ travelrec/similarity.py
@@ -27,6 +27,9 @@ class TemperatureSimilarity(BaseSimiliarity):
 
 
 class GeoFeaturesSimilarity(BaseSimiliarity):
+    """Based on GeoNames codes: https://www.geonames.org/export/codes.html
+    """
+
     def __init__(self):
         super().__init__()
 
@@ -35,6 +38,29 @@ class GeoFeaturesSimilarity(BaseSimiliarity):
 
         for word in words:
             for feature in ["beach", "desert"]:
+                a = wn.synsets(word)[0]
+                b = wn.synsets(feature)[0]
+
+                similarity = a.path_similarity(b)
+                if similarity is not None and similarity > 0.8:
+                    top_words.append(feature)
+
+        return top_words
+
+
+class ActvitySimilarity(BaseSimiliarity):
+    """Based on SLIPO codes: 
+    https://github.com/SLIPO-EU/TripleGeo/blob/master/test/classification/OSM_POI_sample_classification.csv
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def construct_filters(self, words):
+        top_words = []
+        features = [x.lower() for x in ["ARCHERY", "BASEBALL"]]
+        for word in words:
+            for feature in features:
                 a = wn.synsets(word)[0]
                 b = wn.synsets(feature)[0]
 
