bad2da5448fa7e876299af83186bfeb7f29cdfa1
kowaliks <kowal96@gmail.com>
2020-01-11 18:23:53 +0100

    #9 added web interface route

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 import en_core_web_sm
 import json
 from travelrec.recommendations_pipeline import recommendations_pipeline
@@ -8,9 +8,14 @@ app = Flask(__name__)
 nlp = en_core_web_sm.load()
 
 @app.route('/api/recommendations/<query>')
-def index(query):
+def api_recommendations(query):
     recs = recommendations_pipeline(query, nlp)
     return json.dumps(recs)
+
+@app.route('/app/<path:path>')
+def app_path(path):
+    print(path)
+    return send_from_directory('web', path)
 
 if __name__ == "__main__":
     app.run(debug=True)
