a4949f8d9afa12f54b82a88bc1e049500cd30ac1
kowaliks <kowal96@gmail.com>
2020-01-28 12:56:38 +0100

    tests fixed

--- manual_tests/test_get_activities.py
+++ manual_tests/test_get_activities.py
@@ -1,7 +1,7 @@
 from travelrec.sparql import ( get_activities, get_location )
 from manual_tests.coordinates_to_file import coordinates_to_file
 
-latitude, longitude = get_location('Berlin')
+_, latitude, longitude = get_location('Berlin')
 
 res = get_activities(latitude, longitude, 100, ['ZOO'])
 
